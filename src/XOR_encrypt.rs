e9a393f82231fc9fd31e11e86042ac1af2a40598
Ramilatif <latiframi18@gmail.com>
2024-04-14 16:18:50 +0200

    Reading of the binary files

--- src/XOR_encrypt.rs
+++ src/XOR_encrypt.rs
@@ -8,10 +8,10 @@ pub unsafe fn XOR_encrypt(path: &str)  -> std::io::Result<()>{
   //This part of code, can read a file with a path 
   let mut file = File::open(path)?;
  
-    let mut content = String::new();
-    file.read_to_string(&mut content)?;
+    let mut content = Vec::new();
+    file.read_to_end(&mut content)?;
 
-    println!("{}", content);
+    println!("{:?}", content);
 
     Ok(())
 
