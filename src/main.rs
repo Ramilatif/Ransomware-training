bdf44f7cfc2ade30d6e6692429a1da6df9079c96
Ramilatif <latiframi18@gmail.com>
2024-04-17 22:31:19 +0200

    Finally, FLTK works

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,19 @@ mod window_interact;
 use disk_enum::disk_enum;
 use XOR_encrypt::XOR_encrypt;
 use window_interact::window_interact;
-extern crate gtk;
-use gtk::prelude::*;
-use gtk::{ButtonsType, DialogFlags, MessageDialog, MessageType, Window, WindowType};
-use fltk::{app, window};
+
+use fltk::{prelude::*, *};
 
 fn main() {
     unsafe {
-        /*
-        for entry in disk_enum() {
-            XOR_encrypt(&entry);
-        }*/
-        let app = app::App::default();
-    let mut wind = window::Window::new(100, 100, 400, 300, "Fenêtre FLTK");
+        
 
-    wind.end();
-    wind.show();
-
-    app.run().unwrap();
+        
+            let app = app::App::default();
+            let mut my_window = window::Window::new(100, 100, 400, 300, "My Window");
+            my_window.end();
+            my_window.show();
+            app.run().unwrap();
+        
     }
 }
--- src/window_interact.rs
+++ src/window_interact.rs
@@ -1,35 +1,7 @@
-extern crate gtk;
-use gtk::prelude::*;
-use gtk::{ButtonsType, DialogFlags, MessageDialog, MessageType, Window, WindowType};
+
 
 
 pub async unsafe fn window_interact() {
    // Initialise GTK
-   gtk::init().expect("Failed to initialize GTK.");
-
-   // Crée une nouvelle fenêtre
-   let window = Window::new(WindowType::Toplevel);
-   let aha = "yee";
-   window.set_title(&aha);
-   window.set_default_size(350, 70);
-
-   // Crée un dialog de message
-   let dialog = MessageDialog::new(
-       Some(&window),
-       DialogFlags::DESTROY_WITH_PARENT,
-       MessageType::Info,
-       ButtonsType::Ok,
-       "Ceci est un message dans une fenêtre en Rust !",
-   );
-
-   // Connecte le signal pour fermer la fenêtre lors de la fermeture du dialog
-   dialog.connect_response(|dialog, _| {
-       dialog.close();
-   });
-
-   // Affiche la fenêtre et le dialog
-   dialog.show_all();
-
-   // Démarre la boucle principale de l'application
-   gtk::main();
+  
 }
