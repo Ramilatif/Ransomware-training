bf0ed5e693ecd8ced3e385b31a6fafd59ab82a20
Ramilatif <latiframi18@gmail.com>
2024-04-18 00:15:34 +0200

    adding a background

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,44 @@ mod window_interact;
 use disk_enum::disk_enum;
 use XOR_encrypt::XOR_encrypt;
 use window_interact::window_interact;
-
+use fltk::image::{PngImage};
 use fltk::{prelude::*, *};
+use fltk::{app, enums::FrameType, frame::Frame, image::SvgImage, prelude::*, window::Window,text::{TextBuffer, TextDisplay}};
+use fltk::window;
+
 
 fn main() {
     unsafe {
-        
-
-        
             let app = app::App::default();
-            let mut my_window = window::Window::new(100, 100, 400, 300, "My Window");
-            my_window.end();
-            my_window.show();
-            app.run().unwrap();
+            let icon_image = PngImage::load("..\\extra\\img\\icon.png").unwrap();
+            let mut image = PngImage::load("..\\extra\\img\\background.png").unwrap();
+            let (width, height) = (image.w(), image.h());
+            let mut text_display = TextDisplay::new(50, 50, 300, 200, "");
+             let mut buf = text::TextBuffer::default();
+
+            let mut wind = window::Window::new(100, 100, width, height, "Osakaware");
+           
+            
+           // wind.set_resizable(false);
+           
+
+            wind.set_icon(Some(icon_image));
+
+
+ let mut frame = Frame::default().with_size(width, height);
+    frame.set_frame(FrameType::EngravedBox);
+  
+    frame.draw(move |f| {
+        
+        
+        image.draw(f.x(), f.y(), f.w(), f.h());
+    });
+   
+    wind.flush();
+    wind.end();
+    wind.show();
+
+    app.run().unwrap();
         
     }
 }
